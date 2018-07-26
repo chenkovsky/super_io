9a933eb9b6edf54040e3911113c8c0c1ff75ce9b
chenkovsky <chenkov@yeah.net>
2018-07-26 17:16:23 +0800

    change save load to macro

--- src/super_io/save_load.cr
+++ src/super_io/save_load.cr
@@ -1,5 +1,5 @@
 module SuperIO
-  module Save
+  macro save_load
     def to_disk(path : String, format = IO::ByteFormat::LittleEndian) : Void
       File.open(path, "wb") do |f|
         to_io f, format
@@ -9,16 +9,13 @@ module SuperIO
     def save(path : String, format = IO::ByteFormat::LittleEndian) : Void
       to_disk(path, format)
     end
-  end
-
-  module Load
-    def from_disk(path : String, format = IO::ByteFormat::LittleEndian) : self
+    def self.from_disk(path : String, format = IO::ByteFormat::LittleEndian) : self
       File.open(path, "rb") do |f|
         self.from_io f, format
       end
     end
 
-    def load(path : String, format = IO::ByteFormat::LittleEndian) : self
+    def self.load(path : String, format = IO::ByteFormat::LittleEndian) : self
       from_disk(path, format)
     end
   end
